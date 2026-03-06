9790dd75d0505ea2bf82a43d7ebb6eb75f19171d
Brent <brentjames.aberion@cit.edu>
2026-03-06 17:21:45 +0800

    Change class

--- Battle.java
+++ Battle.java
@@ -13,7 +13,7 @@ public class Battle {
         }
 
         if(player.hp > 0)
-            System.out.println("Player wins!");
+            System.out.println("Player afadfdafa wins!");
         else
             System.out.println("Enemy wins!");
 
